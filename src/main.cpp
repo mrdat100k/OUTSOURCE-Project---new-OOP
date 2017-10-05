0e81044aeb75fb8e4a50cd05b4a9cf30fa80c9a5
tuangjv <32361023+tuangjv@users.noreply.github.com>
2017-10-06 01:25:10 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-/*! file chứa chương trình chính
-* chương trình demo: đọc giá trị cảm biến, cập nhật thời gian,
-* hiển thị ra màn hình lcd, nút nhấn để chuyển menu, reset thời gian.
+/* 
+* Functional testing: reading current value, voltage value, power value from INA module,  
+* time update, display content on screen, press button to go next screen, reset timer.
 *
 */
 #include <mbed.h>
@@ -9,26 +9,25 @@
 #include <INAReader.h>
 #include <RTCTimer.h>
 #include <KeyboardController.h>
-/*khởi tạo đối tượng lcdcontroller*/
+/*initialization lcd object*/
 LCDController lcdcontroller(&g_lcd_object);
-/*khởi tạo đối tượng đo lường*/
+/*initialization measuring object */
 INAReader ina_reader(&g_battery_measure_object, &g_pv_measure_object);
-/*khởi tạo đối tượng bàn phím*/
+/*initialization keyboard object*/
 KeyboardController keyboard(SELECT_BUTTON_PIN, SET_BUTTON_PIN, INVERTER_ON_PIN);
-/*khởi tạo đối tượng đồng hồ thời gian thực*/
+/*initialization realtime clock object */
 RTC_Timer rtc_timer;
 int main() {
-    /*hiển thị logo watershed ra màn hình*/
+    /*Display logo watershed on screen*/
     lcdcontroller.showLogo();
     wait(3);
     while(true) 
     {
-        // put your main code here, to run repeatedly:
-        /*quét giá trị đo được từ cảm biến*/
+        /*Reading values from INA module*/
         ina_reader.Scan();
-        /*cập nhật thời gian thực*/
+        /*updating realtime clock*/
         rtc_timer.Update();
-        /*truyền vào đối tượng màn hình các thông số cần hiển thị*/
+        /*updating properties of lcd object */
         lcdcontroller.setBattVolt(ina_reader.getBattVolt());
         lcdcontroller.setBattCurr(ina_reader.getBattCurr());
         lcdcontroller.setBattPower(ina_reader.getBattPower());
@@ -36,7 +35,7 @@ int main() {
         lcdcontroller.setPVCurr(ina_reader.getPVCurr());
         lcdcontroller.setPVPower(ina_reader.getPVPower());
         lcdcontroller.setTime(rtc_timer.GetHour(), rtc_timer.GetMinute(), rtc_timer.GetSecond());
-        /*Cập nhật giá trị ra màn hình, màn hình được điều khiển bằng nút nhấn*/
+        /*selecting screen to display*/
         lcdcontroller.updateScreen(keyboard.menu_index);
     }
 }
